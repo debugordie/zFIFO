65f27b9325c9a216ee6d979f9ab971058ac67356
Yasunori Osana <yasu@prosou.nu>
2019-11-16 02:45:34 +0000

    added FIFO loopback example setup Tcl scripts
    git-svn-id: https://lut.eee.u-ryukyu.ac.jp/svn/zfifo/trunk@5 5098cf71-1ac2-e911-90a1-001999d1b924

---
+++ examples/fifo/zybo/zybo.tcl
@@ -0,0 +1,10 @@
+set_property board_part digilentinc.com:zybo:part0:1.0 [current_project]
+set_property simulator_language Verilog [current_project]
+set_property source_mgmt_mode All [current_project]
+
+source [file join [file dirname [info script]] "design_ps.tcl"]
+
+make_wrapper -files [get_files design_ps.bd] -top
+set BD [file dirname [get_files design_ps.bd]]
+add_files $BD/hdl/design_ps_wrapper.v
+
